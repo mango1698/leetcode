b5f25efddc86a2eb29dbd0474f822bd34d5b9743
codewei <634498594@qq.com>
2023-04-13 20:55:56 +0800

    第一题 两数之和

--- src/No1SumOfTwoNumbers.java
+++
@@ -1,7 +0,0 @@
-/**
- * @author shw
- * @createTime 2023年04月13日 19:54:00
- * @description
- */
-public class No1SumOfTwoNumbers {
-}
---
+++ src/No1TwoSum.java
@@ -0,0 +1,23 @@
+
+
+/**
+ * @author shw
+ * @createTime 2023年04月13日 19:54:00
+ * @description 第一题 两数之和
+ * 遍历数组进行求和，与目标结果进行比较。 时间复杂度O(n²)
+ */
+
+public class No1TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i=0;i<nums.length;i++){
+            for (int j=i+1;j<nums.length;j++){
+                if (nums[i]+nums[j]==target){
+                    result[0]=i;
+                    result[1]=j;
+                }
+            }
+        }
+        return result;
+    }
+}
